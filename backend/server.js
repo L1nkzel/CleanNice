bef31a4ec628227a775d54d6c45e47cf3e1e2400
Semir Pecar <91013287+freemayne@users.noreply.github.com>
2022-12-07 20:53:09 +0100

    database  and backend init

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import "dotenv/config";
+
+
+
+const server = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+server.use(express.json())
+
+
+
+
+server.listen(PORT, () => console.log(`Server started on ${PORT}`))
