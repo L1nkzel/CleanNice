a74592dc031f66171b107265c22d96c0cbeca909
L1nkzel <javalink@protonmail.com>
2022-12-11 18:13:41 +0100

    fixed with routes in header

--- frontend/src/components/ui/Header.jsx
+++ frontend/src/components/ui/Header.jsx
@@ -4,8 +4,7 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 
 import Button from "@mui/material/Button";
-import { Typography } from "@mui/material";
-import { Rowing } from "@mui/icons-material";
+import { Link, Typography } from "@mui/material";
 
 
 export default function Header() {
@@ -13,10 +12,9 @@ export default function Header() {
     <Box sx={{ flexGrow: 1, padding: 2, backgroundColor: "#62926C" }}>
       <Toolbar sx={{ alignItems: "center", justifyContent: "space-between" }}>
         <Box sx={{flexGrow: 0.1,display:'flex',flexDirection:'row', justifyContent:'space-around'}}>
-        <Typography>Min sida</Typography>
-        <Typography>Min sida</Typography>
-        <Typography>Min sida</Typography>
-
+        <Link href="/customer" sx={{fontSize: 20}}>Min sida</Link>
+        <Link  href="/customer/services" sx={{fontSize: 20}}>Tjänster</Link>
+        <Link href="/customer/support" sx={{fontSize: 20}}>Support</Link>
         </Box>
         <Button sx={{ color: "black", boxShadow: 3 }}>Logga ut</Button>
       </Toolbar>
