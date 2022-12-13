8577cd7578b42c4ca1d8298b532d711340f7c917
Semir Pecar <91013287+freemayne@users.noreply.github.com>
2022-12-13 12:25:05 +0100

    margin fix

--- frontend/src/components/booking/RadioButtonsGroup.jsx
+++ frontend/src/components/booking/RadioButtonsGroup.jsx
@@ -15,39 +15,41 @@ export default function RadioButtonsGroup({ selected, setSelected }) {
   }
 
   return (
-    <FormControl sx={{display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    mb: 2,
-    pt:2,
-    
-    borderRadius: 3,
-    boxShadow: 10,
-    backgroundColor: "#BCC7B8",}}>
-      <FormLabel sx={{textAlign:'center', fontSize:20, fontWeight:'bold'}} id="demo-radio-buttons-group-label">Välj en tjänst:</FormLabel>
+    <FormControl>
+      <FormLabel sx={{textAlign:'center', fontSize:20, fontWeight:'bold', mb:5}} id="demo-radio-buttons-group-label">Välj en tjänst:</FormLabel>
       <RadioGroup
         aria-labelledby="demo-radio-buttons-group-label"
         name="radio-buttons-group"
         defaultValue="brons"
         value={selected}
+        sx={{display: "flex",
+    flexDirection: "column",
+    
+    mb: 5,
+    pt:2,
+    
+    borderRadius: 3,
+    boxShadow: 10,
+    backgroundColor: "#BCC7B8",}}
         onChange={handleChange}
       >
+        
+
         <FormControlLabel
           sx={{ mt: 2, ml: 4}}
           value="Brons Städning"
           control={<Radio />}
           label={<Typography sx={{backgroundColor:"#CD7F32", py:0.5, px:1, borderRadius:2, fontWeight:'bold', fontSize:18}} >Brons Städning</Typography>}
-        />
+          />
         <Typography sx={{ ml: 9.5, mb: 4, mr:2 }}>
           Dammsugning, moppning, städ av toaletter och tömning av papperskorgar.
         </Typography>
         <FormControlLabel
-          sx={{ mt: 2, ml: 4 }}
+          sx={{ ml: 4 }}
           value="Silver Städning"
           control={<Radio />}
           label={<Typography sx={{backgroundColor:"#aaa9ad", py:0.5, px:1, borderRadius:2, fontWeight:'bold', fontSize:18}} >Silver Städning</Typography>}
-        />
+          />
         <Typography sx={{ ml: 9.5, mb: 4, mr:2 }}>
           Allt som erbjuds i brons städning samt avdamning och skrivbords
           städning.
@@ -62,6 +64,7 @@ export default function RadioButtonsGroup({ selected, setSelected }) {
           Allt som erbjuds i brons och silver städning samt fönsterputsning.
         </Typography>
       </RadioGroup>
+          
     </FormControl>
   );
 }
