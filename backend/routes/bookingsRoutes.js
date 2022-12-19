fa3c6c909ac85c894bd5c9bedf49b1c3209836cb
Semir Pecar <91013287+freemayne@users.noreply.github.com>
2022-12-19 17:27:35 +0100

    Fixed assign cleaner

--- backend/routes/bookingsRoutes.js
+++ backend/routes/bookingsRoutes.js
@@ -7,7 +7,13 @@ const route = express.Router();
 
 route.get("/", async (req, res) => {
   try {
-    const bookings = await prisma.bookings.findMany({});
+    const bookings = await prisma.bookings.findMany({
+      include:{
+        _count: {
+          select:{employee: true}
+        },}
+
+    });
     res.json(bookings);
   } catch (error) {
     res.json(error);
@@ -72,6 +78,24 @@ route.post("/:id/newBooking", async (req, res) => {
   }
 });
 
+route.patch("/:id/editBookingCleaner", async (req, res) => {
+  const booking = await prisma.bookings.update({
+    where: {
+      bookingId: parseInt(req.params.id),
+    },
+    data: {
+      cleanerName: req.body.cleanerName || undefined,
+      status: req.body.status,
+      employee: {
+        connect: {
+          employeeId: parseInt(req.body.employeeId) || "",
+          email: req.body.email || undefined
+        },
+      },
+    },
+  });
+  res.json(booking);
+});
 
 route.patch("/:id/editBooking", async (req, res) => {
   const booking = await prisma.bookings.update({
@@ -79,7 +103,9 @@ route.patch("/:id/editBooking", async (req, res) => {
       bookingId: parseInt(req.params.id),
     },
     data: {
+      cleanerName: req.body.cleanerName || undefined,
       status: req.body.status,
+     
     },
   });
   res.json(booking);
