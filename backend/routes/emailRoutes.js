79c5082a0979323fd2a216233c4398e1956c0729
L1nkzel <javalink@protonmail.com>
2022-12-29 18:15:21 +0100

    send Invoice to email

--- backend/routes/emailRoutes.js
+++ backend/routes/emailRoutes.js
@@ -1,6 +1,9 @@
 import Sib from "sib-api-v3-sdk";
 import "dotenv/config";
 import express from "express";
+import easyinvoice from "easyinvoice";
+import fs from "fs";
+
 
 const route = express.Router();
 Sib.ApiClient.instance.authentications["api-key"].apiKey = process.env.API_KEY;
@@ -17,7 +20,7 @@ route.post("/newBooking", async (req, res) => {
     .sendTransacEmail({
       subject: "Tack för din bokning!",
       sender: { email: "no-reply@stadafint.se", name: "Städa Fint AB" },
-      to: [{ name: req.body.custName, email: req.body.email }],
+      to: [{ name: req.body.custName, email: "javalink@protonmail.com" }],
       htmlContent: htmlContent,
     })
     .then(
@@ -99,6 +102,120 @@ route.post("/assignCleaner", async (req, res) => {
       sender: { email: "no-reply@stadafint.se", name: "Städa Fint AB" },
       to: [{ name: req.body.custName, email: req.body.email }],
       htmlContent: htmlContent,
+    })
+    .then(
+      function (data) {
+        console.log(data);
+      },
+      function (error) {
+        console.error(error);
+      }
+    );
+  res.json(email);
+});
+route.post("/invoice", async (req, res) => {
+
+  let price;
+  const date = new Date();
+  const dueDate = new Date(new Date().setDate(date.getDate() + 30));
+ 
+  if(req.body.cleaningService === "Brons städning") {
+   price = 2000;
+  } else if(req.body.cleaningService === "Silver städning") {
+   price = 2800;
+  } else {
+   price = 4000;
+
+  } 
+
+  const htmlContent = {
+    // Customize enables you to provide your own templates
+    // Please review the documentation for instructions and examples
+    customize: {
+      //  "template": fs.readFileSync('template.html', 'base64') // Must be base64 encoded html
+    },
+
+    // Your own data
+    sender: {
+      company: "Städa Fint AB",
+      address: "Nydalavägen 5a",
+      zip: "12321",
+      city: "Malmö",
+      country: "Sweden",
+      //"custom1": "custom value 1",
+      //"custom2": "custom value 2",
+      //"custom3": "custom value 3"
+    },
+    // Your recipient
+    client: {
+      company: "Mottagare",
+      address: req.body.custName,
+      zip: req.body.company,
+      city: req.body,
+      country: req.body.address,
+      // "custom1": "custom value 1",
+      // "custom2": "custom value 2",
+      // "custom3": "custom value 3"
+    },
+    information: {
+      // Invoice number
+      number: req.body.bookingId,
+      // Invoice data
+      date: date.toLocaleDateString("sv-SE"),
+      // Invoice due date
+      "due-date": dueDate.toLocaleDateString("sv-SE")
+    },
+    // The products you would like to see on your invoice
+    // Total values are being calculated automatically
+    products: [
+      {
+        quantity: 1,
+        description: req.body.cleaningService,
+        "tax-rate": 25,
+        price: price, 
+      }
+    ],
+    // The message you would like to display on the bottom of your invoice
+    "bottom-notice": "Vänligen betala fakturan inom 30 dagar.",
+    // Settings to customize your invoice
+    settings: {
+      currency: "SEK", // See documentation 'Locales and Currency' for more info. Leave empty for no currency.
+      // "locale": "nl-NL", // Defaults to en-US, used for number formatting (See documentation 'Locales and Currency')
+      // "tax-notation": "gst", // Defaults to 'vat'
+      // "margin-top": 25, // Defaults to '25'
+      // "margin-right": 25, // Defaults to '25'
+      // "margin-left": 25, // Defaults to '25'
+      // "margin-bottom": 25, // Defaults to '25'
+      // "format": "A4", // Defaults to A4, options: A3, A4, A5, Legal, Letter, Tabloid
+      // "height": "1000px", // allowed units: mm, cm, in, px
+      // "width": "500px", // allowed units: mm, cm, in, px
+      // "orientation": "landscape", // portrait or landscape, defaults to portrait
+    },
+    // Translate your invoice to your preferred language
+    translate: {
+      invoice: "FAKTURA", // Default to 'INVOICE'
+      number: "OCR-nr", // Defaults to 'Number'
+      date: "Datum", // Default to 'Date'
+      "due-date": "Utgångsdatum", // Defaults to 'Due Date'
+      "subtotal": "Exl moms", // Defaults to 'Subtotal'
+      "products": "Tjänster", // Defaults to 'Products'
+      "quantity": "Antal", // Default to 'Quantity'
+      price: "Pris", // Defaults to 'Price'
+      "product-total": "Totalt", // Defaults to 'Total'
+      total: "Summa", // Defaults to 'Total'
+    },
+  };
+
+  const htmlContent2 = await easyinvoice.createInvoice(htmlContent);
+  console.log(htmlContent2.pdf.toString(""));
+
+  const email = new Sib.TransactionalEmailsApi()
+    .sendTransacEmail({
+      subject: "Städning tilldelad!",
+      sender: { email: "no-reply@stadafint.se", name: "Städa Fint AB" },
+      to: [{ name: req.body.custName, email: req.body.email }],
+      htmlContent: "Hej",
+      attachment: [{"name": "faktura.pdf", "content": htmlContent2.pdf}]
     })
     .then(
       function (data) {
