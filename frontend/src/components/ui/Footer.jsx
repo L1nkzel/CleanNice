8cc27a3b8cd37d49ee32370b0ff42a51aa663c9c
L1nkzel <javalink@protonmail.com>
2023-01-02 14:40:34 +0100

    New logo & created CookiesPopup

---
+++ frontend/src/components/ui/Footer.jsx
@@ -0,0 +1,13 @@
+import { Box, Link } from '@mui/material'
+import React from 'react'
+
+function Footer() {
+  return (
+    <Box sx={{position: 'fixed',
+    bottom: 0, m:1}}>
+            <Link fontSize={13} href="https://www.freepik.com/free-vector/hand-drawn-cleaning-service-logo_21250282.htm#query=cleaning%20service%20logo&position=2&from_view=search&track=ais">Logo by pikisuperstar</Link> 
+    </Box>
+  )
+}
+
+export default Footer
