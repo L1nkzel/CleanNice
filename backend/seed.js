713624957ed46e140f14235fb676bc141e6be114
Semir Pecar <91013287+freemayne@users.noreply.github.com>
2023-10-25 14:43:27 +0200

    prepared for deployment

---
+++ backend/seed.js
@@ -0,0 +1,69 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function seedData() {
+  try {
+    // Insert mock data for customers
+    await prisma.customer.createMany({
+      data: [
+        {
+          companyName: 'AB Svenska Företaget',
+          orgNr: '556677-8899',
+          phoneNumber: '+46 123 456 789',
+          email: 'kund1@example.com',
+          adress: 'Storgatan 1',
+          custName: 'Anna Andersson',
+          password: '123', // Replace with a hashed password
+          forceChangePass: 'false',
+        },
+        {
+          companyName: 'Firma Sverige AB',
+          orgNr: '123456-7890',
+          phoneNumber: '+46 987 654 321',
+          email: 'kund2@example.com',
+          adress: 'Lillgatan 5',
+          custName: 'Erik Eriksson',
+          password: '123', // Replace with a hashed password
+          forceChangePass: 'false',
+        },
+      ],
+    });
+
+    // Insert mock data for employees
+    await prisma.employee.createMany({
+      data: [
+        {
+          personalNumber: '198765-4321',
+          accountNumber: 'SE123456',
+          phoneNumber: '+46 111 222 333',
+          email: 'admin@example.com',
+          adress: 'Bokvägen 7',
+          role: 'Admin',
+          employeeName: 'Maria Karlsson',
+          password: '123', 
+          forceChangePass: 'no',
+        },
+        {
+          personalNumber: '198754-3210',
+          accountNumber: 'SE654321',
+          phoneNumber: '+46 444 555 666',
+          email: 'employee@example.com',
+          adress: 'Ekbacken 3',
+          role: 'Employee',
+          employeeName: 'Jonas Nilsson',
+          password: '123', 
+          forceChangePass: 'no',
+        },
+      ],
+    });
+
+    console.log('Data seeded successfully.');
+  } catch (error) {
+    console.error('Error seeding data:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+seedData();
--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,11 @@ import employeeRoutes from "./routes/employeeRoutes.js";
 import bookingsRoutes from "./routes/bookingsRoutes.js";
 import emailRoutes from "./routes/emailRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
+import { PrismaClient } from "@prisma/client";
 
 
+
+const prisma = new PrismaClient();
 const server = express();
 
 const PORT = process.env.PORT || 5000;
@@ -44,6 +47,26 @@ server.use("/api/customer", isAuthenticated, customerRoutes);
 
 server.use("/api/employee", isAuthenticated, employeeRoutes);
 server.use("/api/bookings", isAuthenticated, bookingsRoutes);
-server.use("/api/email", isAuthenticated, emailRoutes);
+//server.use("/api/email", isAuthenticated, emailRoutes);
 
 server.listen(PORT, () => console.log(`Server started on ${PORT}`));
+
+async function main() {
+
+
+  await prisma.$connect();
+
+  const users = await prisma.user.findMany();
+  console.log(users);
+
+
+  await prisma.$disconnect();
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
